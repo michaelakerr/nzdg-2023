a3e3fae90bdf2c748edfed94b134b757f6bd6672
michaelakerr <michaela.kerrr@gmail.com>
2023-06-06 11:28:10 +1200

    Update streamlit secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
-import streamlit as st
+import json
 
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
+import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
+
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds)
+
+# Create a reference to the Google post.
+doc_ref = db.collection("players").document("UiyFREqNMHG62aTNLEij")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
