5f1c94c989c1e88a13d004480c8f4ae94930345c
MichaelaKerr <michaela.kerr@countiesenergy.co.nz>
2024-05-06 19:17:34 +1200

    Remove uneeded import

--- pages/2_Tour_Events.py
+++ pages/2_Tour_Events.py
@@ -7,7 +7,6 @@ import json
 from google.cloud import firestore
 from google.oauth2 import service_account
 
-from parser_for_pdga import export_players_to_csv
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
