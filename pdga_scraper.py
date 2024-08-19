8efc19c790265a4e1da428e9ce52f30153f5791a
MichaelaKerr <michaela.kerr@countiesenergy.co.nz>
2024-08-19 14:49:22 +1200

    Update to majors or minors being 0

--- pdga_scraper.py
+++ pdga_scraper.py
@@ -63,25 +63,40 @@ def get_all_tournaments(db):
     tournaments = db.collection(TOURNAMENT_TABLE_DB).order_by("order").stream()
     return tournaments
 
-def get_total_points(db, player):
-    majors = db.collection_group(TOURNAMENT_TABLE_DB).where(
-    filter=FieldFilter("major", "==", True)
-    ).stream()
 
-    minors = db.collection_group(TOURNAMENT_TABLE_DB).where(
-    filter=FieldFilter("major", "==", False)
-    ).stream()
+def get_total_points(db, player):
+    majors = (
+        db.collection_group(TOURNAMENT_TABLE_DB)
+        .where(filter=FieldFilter("major", "==", True))
+        .stream()
+    )
+
+    minors = (
+        db.collection_group(TOURNAMENT_TABLE_DB)
+        .where(filter=FieldFilter("major", "==", False))
+        .stream()
+    )
+
+    majors_list = list(majors)
+    majors_count = len(majors_list)
+    minors_list = list(minors)
+    minors_count = len(minors_list)
+
     total = 0
     major_points_list = []
     minor_points_list = []
 
-    if majors.count() > 0:
+    # get major and minor scores
+
+    if majors_count > 0:
         for m in majors:
             if str(m.id) in player:
                 major_points_list.append(player[m.id])
 
         # sort and take top 2 of major scores
-        major_points_list = sorted(major_points_list, key = lambda x:float(x), reverse=True)
+        major_points_list = sorted(
+            major_points_list, key=lambda x: float(x), reverse=True
+        )
 
         if len(major_points_list) >= 2:
             major_points_list = major_points_list[:2]
@@ -92,7 +107,7 @@ def get_total_points(db, player):
         major_points_list = [x for x in major_points_list if x != 0]
         major_points_list = [x for x in major_points_list if x != None]
 
-    if minors.count() > 0:
+    if minors_count > 0:
         for m in minors:
             if str(m.id) in player:
                 minor_points_list.append(player[m.id])
@@ -100,8 +115,8 @@ def get_total_points(db, player):
     # combine major and minor lists
     total_points = major_points_list + minor_points_list
     # get top 6 points
-    total_points = sorted(total_points, key = lambda x:float(x), reverse = True)
-    if (len(total_points) > 6):
+    total_points = sorted(total_points, key=lambda x: float(x), reverse=True)
+    if len(total_points) > 6:
         total_points = total_points[:6]
 
     # add total points together
@@ -111,113 +126,129 @@ def get_total_points(db, player):
 
 
 def create_player(db, tournament, player):
-    key = str(player["PDGA#"]).strip(".0") + "_" + \
-        player['Name'].replace(" ", "") + "_"+str(player['Player_Group'])
+    key = (
+        str(player["PDGA#"]).strip(".0")
+        + "_"
+        + player["Name"].replace(" ", "")
+        + "_"
+        + str(player["Player_Group"])
+    )
     doc_ref = db.collection(PLAYER_TABLE_DB).document(key)
     doc = doc_ref.get()
     if doc.exists:
         player_info = doc.to_dict()
         player_info[str(tournament)] = player["Points"]
         total_points = get_total_points(db, player_info)
-        doc_ref.set({
-            str(tournament): player["Points"],
-            "total": total_points
-        }, merge=True)
-        print(f'Player data: {doc.to_dict()}')
+        doc_ref.set(
+            {str(tournament): player["Points"], "total": total_points}, merge=True
+        )
+        print(f"Player data: {doc.to_dict()}")
     else:
-        print('No such Player!')
+        print("No such Player!")
         doc_ref = db.collection(PLAYER_TABLE_DB).document(key)
-        doc_ref.set({
-            "name": player['Name'],
-            "pdga_number": player['PDGA#'],
-            "key": key,
-            'Player_Group': player['Player_Group'],
-            "tour_division": map_tour_divisions[player['Div']],
-            "scoring_group": player['Player_Group'],
-            "total": player["Points"],
-            str(tournament).replace(" ", "_"): player["Points"]
-        }, merge=True)
+        doc_ref.set(
+            {
+                "name": player["Name"],
+                "pdga_number": player["PDGA#"],
+                "key": key,
+                "Player_Group": player["Player_Group"],
+                "tour_division": map_tour_divisions[player["Div"]],
+                "scoring_group": player["Player_Group"],
+                "total": player["Points"],
+                str(tournament).replace(" ", "_"): player["Points"],
+            },
+            merge=True,
+        )
+
 
 # Define a custom ranking function
 def custom_rank(group):
     # First rank by 'par', then by 'place'
-    group = group.sort_values(by=['Par', 'Place']).reset_index(drop=True)
+    group = group.sort_values(by=["Par", "Place"]).reset_index(drop=True)
 
     # Add a 'rank' column
-    group['Rank'] = group.index + 1
+    group["Rank"] = group.index + 1
 
     return group
+
 
 def create_players_and_points(tournament_name, url, tour_points):
     response = requests.get(url)
 
-    group1 = ['MPO']
-    group2 = ['MP40', 'MP50']
-    group3 = ['MA1', 'FPO']
-    group4 = ['MA2']
-    group5 = ['MA40', 'MA50']
-    group6 = ['MA3', 'MA60',  'MA4', 'MJ18', 'MJ12', 'MA70']
-    group7 = ['FA1',  'FA40']
-    group8 = ['FA50', 'FA60', 'FA2', 'FA3', 'FA4', 'FJ18' , 'FJ12']
+    group1 = ["MPO"]
+    group2 = ["MP40", "MP50"]
+    group3 = ["MA1", "FPO"]
+    group4 = ["MA2"]
+    group5 = ["MA40", "MA50"]
+    group6 = ["MA3", "MA60", "MA4", "MJ18", "MJ12", "MA70"]
+    group7 = ["FA1", "FA40"]
+    group8 = ["FA50", "FA60", "FA2", "FA3", "FA4", "FJ18", "FJ12"]
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, "html.parser")
 
     # Get divs in tournament
-    divisions = soup.find_all('h3', {'class': "division"})
+    divisions = soup.find_all("h3", {"class": "division"})
     list_of_divs = []
 
     for row in divisions:
-        list_of_divs.append(row.get_text().split(' · ')[0])
+        list_of_divs.append(row.get_text().split(" · ")[0])
 
     # Get players
-    page = soup.find_all('details')
+    page = soup.find_all("details")
     players = pd.read_html(StringIO(str(page)))
 
     for index, div in enumerate(players):
-        div['Div'] = list_of_divs[index]
+        div["Div"] = list_of_divs[index]
         if list_of_divs[index] in group1:
-            div['Player_Group'] = 1
+            div["Player_Group"] = 1
         elif list_of_divs[index] in group2:
-            div['Player_Group'] = 2
+            div["Player_Group"] = 2
         elif list_of_divs[index] in group3:
-            div['Player_Group'] = 3
+            div["Player_Group"] = 3
         elif list_of_divs[index] in group4:
-            div['Player_Group'] = 4
+            div["Player_Group"] = 4
         elif list_of_divs[index] in group5:
-            div['Player_Group'] = 5
+            div["Player_Group"] = 5
         elif list_of_divs[index] in group6:
-            div['Player_Group'] = 6
+            div["Player_Group"] = 6
         elif list_of_divs[index] in group7:
-            div['Player_Group'] = 7
+            div["Player_Group"] = 7
         elif list_of_divs[index] in group8:
-            div['Player_Group'] = 8
+            div["Player_Group"] = 8
         else:
-            div['Player_Group'] = 0
+            div["Player_Group"] = 0
 
     # consolidate all the divs
     df = pd.concat(players)
     df = df[df.Total != "DNF"]
 
-    df = df.drop(['Points', 'Rating', 'Rd1', 'Rd2', 'Rd3',
-                 'Rd4', 'Total', 'Prize (USD)'], axis=1, errors='ignore')
-    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    df = df.drop(
+        ["Points", "Rating", "Rd1", "Rd2", "Rd3", "Rd4", "Total", "Prize (USD)"],
+        axis=1,
+        errors="ignore",
+    )
+    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
 
-    df['Par'] = pd.to_numeric(
-        df['Par'], errors='coerce', downcast='integer').fillna(0)
+    df["Par"] = pd.to_numeric(df["Par"], errors="coerce", downcast="integer").fillna(0)
     # Drop if DNF in par
-    df = df.dropna(subset=['Par'])
-    df = df.groupby('Player_Group', group_keys=False).apply(custom_rank)
+    df = df.dropna(subset=["Par"])
+    df = df.groupby("Player_Group", group_keys=False).apply(custom_rank)
 
     # calculate points
-    count_players_in_divs = df.groupby(['Player_Group'])['Par'].count()
+    count_players_in_divs = df.groupby(["Player_Group"])["Par"].count()
 
     # points
-    #tour_points == 1+ (maxtourpoints-1) x (scoring group player count - ranking) / (scoring group player count-1)
-    df['Points'] = df.apply(lambda x: 1+(tour_points-1)*(count_players_in_divs[x['Player_Group']
-                                                                               ]-x['Rank'])/(count_players_in_divs[x['Player_Group']]-1), axis=1)
+    # tour_points == 1+ (maxtourpoints-1) x (scoring group player count - ranking) / (scoring group player count-1)
+    df["Points"] = df.apply(
+        lambda x: 1
+        + (tour_points - 1)
+        * (count_players_in_divs[x["Player_Group"]] - x["Rank"])
+        / (count_players_in_divs[x["Player_Group"]] - 1),
+        axis=1,
+    )
 
     # rearrange columns
-    df = df[['PDGA#', 'Name', 'Div', 'Par', 'Player_Group', 'Rank', 'Points']]
+    df = df[["PDGA#", "Name", "Div", "Par", "Player_Group", "Rank", "Points"]]
 
-    df.to_csv(tournament_name + '.csv', index=False)
+    df.to_csv(tournament_name + ".csv", index=False)
     return df
