8214d98a89283c7b83d018dd7cc743ec47d94453
MichaelaKerr <michaela.kerr@countiesenergy.co.nz>
2024-10-10 12:15:08 +1300

    Update admin logging in and out

--- pages/4_Admin.py
+++ pages/4_Admin.py
@@ -8,10 +8,14 @@ from streamlit_sortables import sort_items
 from google.cloud import firestore
 from google.oauth2 import service_account
 from yaml.loader import SafeLoader
-from admin_tasks import add_tournament_and_players, rearrange_tournament_order, remove_tournament_and_player_points
+from admin_tasks import (
+    add_tournament_and_players,
+    rearrange_tournament_order,
+    remove_tournament_and_player_points,
+)
 from pdga_scraper import get_all_tournaments
 
-with open('credentials.yaml') as file:
+with open("credentials.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 key_dict = json.loads(st.secrets["textkey2"])
@@ -19,85 +23,92 @@ creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds)
 
 authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
+    config["credentials"],
+    config["cookie"]["name"],
+    config["cookie"]["key"],
+    config["cookie"]["expiry_days"],
 )
 
-name, authentication_status, username = authenticator.login('main')
+try:
+    name, authentication_status, username = authenticator.login(
+        location="main", form_name="admin"
+    )
 
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    if username == 'admin':
-        tournaments = get_all_tournaments(db)
-        tournaments_list = list(map(lambda x: x.to_dict(), tournaments))
-        tournament_df = pd.DataFrame(tournaments_list)
-        max_value = 1
-        
-        # sort df by order column
-        if len(tournaments_list) > 0:
-            default_order = tournament_df.sort_values(by="order")
-            max_value = default_order['order'].max() +1
+    if authentication_status:
+        authenticator.logout(button_name="Logout", key="main")
+        if username == "admin":
+            tournaments = get_all_tournaments(db)
+            tournaments_list = list(map(lambda x: x.to_dict(), tournaments))
+            tournament_df = pd.DataFrame(tournaments_list)
+            max_value = 1
 
-        st.write(f'Welcome *{name}*')
-        st.title('Add a Tournament')
+            # sort df by order column
+            if len(tournaments_list) > 0:
+                default_order = tournament_df.sort_values(by="order")
+                max_value = default_order["order"].max() + 1
 
-        name = st.text_input("Tournament Name")
-        url = st.text_input("Tournament Url")
-        points = st.number_input("Points")
-        major = st.checkbox("Is this a major tournament?")
-        tournament_order = st.number_input("Tournament order", value=max_value)
+            st.write(f"Welcome *{name}*")
+            st.title("Add a Tournament")
 
-       # process_non_pdga = st.button("Process non PDGA tournament")
+            tournament_name = st.text_input("Tournament Name")
+            url = st.text_input("Tournament Url")
+            points = st.number_input("Points")
+            major = st.checkbox("Is this a major tournament?")
+            tournament_order = st.number_input("Tournament order", value=max_value)
 
-        submit = st.button("Submit tournament")
+            # process_non_pdga = st.button("Process non PDGA tournament")
 
-        st.divider()
+            submit = st.button("Submit tournament")
 
-        st.title("Remove a tournament")
-        tournaments = get_all_tournaments(db)
+            st.divider()
 
-        names = []
+            st.title("Remove a tournament")
+            tournaments = get_all_tournaments(db)
 
-        #setup tournament names
-        for tournament in tournaments:
-            names.append(tournament.id)
+            names = []
 
-        remove_tournament = st.selectbox("Select which tournament to remove", names)
-        remove = st.button("Remove tournament")
+            # setup tournament names
+            for tournament in tournaments:
+                names.append(tournament.id)
 
-        st.divider()
-        st.title('Rearrange Tournaments')
+            remove_tournament = st.selectbox("Select which tournament to remove", names)
+            remove = st.button("Remove tournament")
 
-        tournament_sorter = get_all_tournaments(db)
+            st.divider()
+            st.title("Rearrange Tournaments")
 
-        names_in_order = []
+            tournament_sorter = get_all_tournaments(db)
 
-        #setup tournament names
-        for tournament in tournament_sorter:
-            names_in_order.append(tournament.id)
+            names_in_order = []
 
-        sorted_items = sort_items(names_in_order)
+            # setup tournament names
+            for tournament in tournament_sorter:
+                names_in_order.append(tournament.id)
 
-        rearrange_tournaments = st.button("Rearrange tournaments")
+            sorted_items = sort_items(names_in_order)
 
-        #Remove tournaments
-        if remove_tournament and remove:
-            remove_tournament_and_player_points(db, remove_tournament)
+            rearrange_tournaments = st.button("Rearrange tournaments")
 
-        # Once the user has submitted, upload it to the database
-        if name and url and points and submit:
-            add_tournament_and_players(db, name, url, points, major, tournament_order)
+            # Remove tournaments
+            if remove_tournament and remove:
+                remove_tournament_and_player_points(db, remove_tournament)
 
-        if rearrange_tournaments and sorted_items: 
-            rearrange_tournament_order(db, sorted_items)
+            # Once the user has submitted, upload it to the database
+            if tournament_name and url and points and submit:
+                add_tournament_and_players(
+                    db, tournament_name, url, points, major, tournament_order
+                )
 
-        #if process_non_pdga:
-            #add_tournament_and_players_non_pdga(db)
-        
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+            if rearrange_tournaments and sorted_items:
+                rearrange_tournament_order(db, sorted_items)
 
+            # if process_non_pdga:
+            # add_tournament_and_players_non_pdga(db)
+
+    elif authentication_status == False:
+        st.error("Username/password is incorrect")
+    elif authentication_status == None:
+        st.warning("Please enter your username and password")
+
+except Exception as e:
+    st.error(e)
